2622ca2ba40d45fa4ac1dd84cd80449a544a750a
Eckhart <spalding@email.arizona.edu>
2019-06-25 12:50:39 -0700

    join bug fixing

--- modules/basic_red.py
+++ modules/basic_red.py
@@ -842,10 +842,10 @@ def main():
     #print(kludge_dir)
     #print(list(glob.glob(kludge_dir + "*.fits")))
     names = [os.path.basename(x) for x in kludge_glob]
-    names = np.array(names)
+    #names = np.array(names)
     print(names)
     ramp_subted_03_name_array = list(os.path.join(ramp_subted_03_directory,*names))
-    print(ramp_subted_03_name_array)
+    #print(ramp_subted_03_name_array)
     ### END MOMENTARY KLUDGE
 
     '''

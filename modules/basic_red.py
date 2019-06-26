Trying to fix overflow bug

modules/basic_red.py
@@ -730,7 +730,7 @@ class CookieCutout:
         # we need this condition in the if-elif further below)
         radius_from_host = int(self.buffer_fac*self.ao_ctrl_pix)
         if np.logical_or((psf_loc[0]-radius_from_host < 0),
-                         (radius_from_host-psf_loc[0] > (np.shape(sciImg)[0]-psf_loc[0]))):
+                         (radius_from_host-psf_loc[0]) > (np.shape(sciImg)[0]-psf_loc[0])):
 
             print("Overflow in frame " + abs_sci_name)
 
@@ -795,7 +795,8 @@ class CookieCutout:
             #fits.writeto(filename="testing_nan_below.fits",data=np.add(cookie_mask1,cookie_mask2),overwrite=True)
 
         # case of overflow above the readout (i.e., the cookie cutout extends beyond the top of the readout)
-        elif (psf_loc_old[0]+radius_from_host > sciImg_shape_old[0]):
+        elif (radius_from_host-psf_loc[0]) > (np.shape(sciImg)[0]-psf_loc[0]):
+            
             overflow_above = np.abs((radius_from_host+psf_loc[0]) - sciImg_shape_old[0]) # number of pixels overflow
             # kludge to replace overflow region with NaNs
             print("Replacing some array above-overflow with NaNs...")
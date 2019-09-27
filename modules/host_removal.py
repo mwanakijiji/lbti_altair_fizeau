b2d9559b4f09b40806e230c5843af864368e4df6
Eckhart <spalding@email.arizona.edu>
2019-09-27 11:39:46 -0700

    Troubleshooting PCA decomp

--- modules/host_removal.py
+++ modules/host_removal.py
@@ -224,6 +224,9 @@ class HostRemovalCube:
                 ## TEST HERE
                 if slice_num == 100:
                     sci = np.ones(np.shape(sci))
+                if slice_num == 200:
+                    sci = np.flip(sci,axis=0)
+                ## END TEST
                 fit_host_star = fit_pca_star(self.pca_basis_cube_host_star, sci, no_mask, n_PCA=100)
             except:
                 print("PCA fit to slice number " + str(slice_num) + " failed; skipping.")

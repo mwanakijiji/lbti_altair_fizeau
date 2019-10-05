f5318443e096b215aeab1b0754ea30c24c5d5426
Eckhart <spalding@email.arizona.edu>
2019-10-05 12:54:30 -0700

    test PCA regions

--- modules/host_removal.py
+++ modules/host_removal.py
@@ -353,6 +353,18 @@ class HostRemovalCube:
                     '''
                     ## END TEST
 
+                    ## TEST: WRITE OUT
+                    if mask_slice_num == len(self.abs_region_mask):
+                        hdu = fits.PrimaryHDU(cube_PCA_recon_regions_1_frame)
+                        hdulist = fits.HDUList([hdu])
+                        hdu.writeto("junk_cube_PCA_recon_regions_1_frame_"+str(mask_slice_num)+".fits", clobber=True)
+
+                        hdu = fits.PrimaryHDU(cube_host_subt_regions_1_frame)
+                        hdulist = fits.HDUList([hdu])
+                        hdu.writeto("junk_cube_host_subt_regions_1_frame_"+str(mask_slice_num)+".fits", clobber=True)
+                        break
+                    ## END TEST
+
                 # anything that is still zero in the recon regions cube, turn it to nan
                 cube_PCA_recon_regions_1_frame[cube_PCA_recon_regions_1_frame == 0] = np.nan
                 # take median of reconstructed frame

4a0d621ca6455d6d57cede3d55d566ca8fa987c6
Eckhart <spalding@email.arizona.edu>
2019-10-26 10:29:22 -0700

    Real fake planets

--- modules/injection_ADI.py
+++ modules/injection_ADI.py
@@ -782,13 +782,13 @@ def main():
     for NO injection of fake planets (and only host star removal and ADI), set rad_asec equal to zero and the others to one element each
     i.e, {"angle_deg_EofN": [0.], "rad_asec": [0.], "ampl_linear_norm": [0.]}
     '''
-
+    '''
     fake_params_pre_permute = {"angle_deg_EofN": [0.], "rad_asec": [0.], "ampl_linear_norm": [0.]}
     '''
     fake_params_pre_permute = {"angle_deg_EofN": [270.],
                                "rad_asec": [0.5, 1.0, 1.5],
                                "ampl_linear_norm": [1e-3]}
-    '''
+
     # permutate values of fake planet parameters to get all possible combinations
     keys, values = zip(*fake_params_pre_permute.items())
     experiments = [dict(zip(keys, v)) for v in itertools.product(*values)]

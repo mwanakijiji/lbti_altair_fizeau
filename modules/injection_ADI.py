dedd6effacb77cbad87736c7a468c8ecefbca7b8
Eckhart <spalding@email.arizona.edu>
2020-04-06 18:36:25 -0700

    FWHM*[2,3]]

--- modules/injection_ADI.py
+++ modules/injection_ADI.py
@@ -883,7 +883,7 @@ def main(inject_iteration=None):
         ps_asec_per_pix = np.float(config["instrum_params"]["LMIR_PS"]) # plate scale
         #A = np.arange(2,21) # FWHM numbers from center to edge of frame (FULL THING)
         #A = np.array([2,3,4,5,7,9,12,15,19]) # more sparse design as R increases (FULL THING)
-        A = np.array([15,19]) # (SUBSET)
+        A = np.array([2,3]) # (SUBSET)
         B = fwhm_pix*ps_asec_per_pix*A # array of angular offsets where fake planets are to be injected
         fake_params_pre_permute = {"angle_deg_EofN": [0.],
                                "rad_asec": list(B),
@@ -1254,6 +1254,7 @@ def main(inject_iteration=None):
     mask_406x406_rings_4quad_fits_complete.fits
     mask_406x406_rings_4quad_fits_truncated.fits
     mask_406x406_rings_4quad_fits_coarse_20200404.fits
+    mask_406x406_center_strip_lamb_over_B.fits
     '''
 
     ## ## BEGIN SERIAL TEST

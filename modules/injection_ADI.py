0161b172e663a52d8c6e6e6b687f305bba6925b7
Eckhart <spalding@email.arizona.edu>
2020-04-12 13:35:10 -0700

    FWHM*9, 0 and 120 degEofN

--- modules/injection_ADI.py
+++ modules/injection_ADI.py
@@ -883,11 +883,13 @@ def main(inject_iteration=None):
         ps_asec_per_pix = np.float(config["instrum_params"]["LMIR_PS"]) # plate scale
         #A = np.arange(2,21) # FWHM numbers from center to edge of frame (FULL THING)
         #A = np.array([2,3,4,5,7,9,12,15,19]) # more sparse design as R increases (FULL THING)
-        A = np.array([15]) # (SUBSET)
+        A = np.array([9]) # (SUBSET)
         B = fwhm_pix*ps_asec_per_pix*A # array of angular offsets where fake planets are to be injected
-        fake_params_pre_permute = {"angle_deg_EofN": [240.],
+
+        fake_params_pre_permute = {"angle_deg_EofN": [0.,120.],
                                "rad_asec": list(B),
-                               "ampl_linear_norm": [0.00013]}
+                               "ampl_linear_norm": [0.0001]}
+
         '''
         fake_params_pre_permute = {"angle_deg_EofN": [0.],
                                "rad_asec": [0.20,0.50,0.80,1.10,1.40,1.70],
@@ -904,7 +906,6 @@ def main(inject_iteration=None):
 
         # add in the initial amplitudes after the permutations have been made
         experiments = [dict(item, **{'ampl_linear_norm_0':item['ampl_linear_norm']}) for item in experiments]
-
 
         ### OPTION 2: MANUALLY SET ALL VALUES
         ### (BE SURE TO COMMENT OUT OPTION 1!)

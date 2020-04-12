4971557abbc9f26860c7427b2a588e5f373bd557
Eckhart <spalding@email.arizona.edu>
2020-04-11 17:18:32 -0700

    FWHM*15, 0 degEofN

--- modules/injection_ADI.py
+++ modules/injection_ADI.py
@@ -885,7 +885,7 @@ def main(inject_iteration=None):
         #A = np.array([2,3,4,5,7,9,12,15,19]) # more sparse design as R increases (FULL THING)
         A = np.array([15]) # (SUBSET)
         B = fwhm_pix*ps_asec_per_pix*A # array of angular offsets where fake planets are to be injected
-        fake_params_pre_permute = {"angle_deg_EofN": [120.],
+        fake_params_pre_permute = {"angle_deg_EofN": [0.],
                                "rad_asec": list(B),
                                "ampl_linear_norm": [0.00013]}
         '''

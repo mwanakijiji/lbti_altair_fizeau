77b8812f66e350fdf9463310cbac1e9da9f91a60
Eckhart <spalding@email.arizona.edu>
2020-05-15 15:52:09 -0700

    Several FWHM with recentering in lambda over B regime

--- modules/injection_ADI.py
+++ modules/injection_ADI.py
@@ -895,7 +895,7 @@ def main(inject_iteration=None):
         ps_asec_per_pix = np.float(config["instrum_params"]["LMIR_PS"]) # plate scale
         #A = np.arange(2,21) # FWHM numbers from center to edge of frame (FULL THING)
         #A = np.array([2,3,4,5,7,9,12,15,19]) # more sparse design as R increases (FULL THING)
-        A = np.array([0.1]) # (SUBSET)
+        A = np.array([0.1,0.2,0.3,0.4,0.5,0.6,1.,2.,3.,4.,5.]) # (SUBSET)
         B = fwhm_pix*ps_asec_per_pix*A # array of angular offsets where fake planets are to be injected
 
         fake_params_pre_permute = {"angle_deg_EofN": [129.68],
@@ -1207,7 +1207,7 @@ def main(inject_iteration=None):
                                                   + config["file_names"]["MEDIAN_SCI_FRAME"]),
         write_name_abs_host_star_PCA = str(config["data_dirs"]["DIR_PCA_CUBES_PSFS"]
                                            + "psf_PCA_vector_cookie_all_A_and_D_frames_pcaNum_100_host_resids.fits"),
-        recenter_option=False)
+        recenter_option=True)
 
     '''
     Note that tesselation region options are

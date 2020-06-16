554cc8a6599379d5ea4fa55ee1200e395e2d6ac5
Eckhart <spalding@email.arizona.edu>
2020-06-16 13:58:45 -0700

    lambda_D_fwhm4_240degEofN_001

--- modules/injection_ADI.py
+++ modules/injection_ADI.py
@@ -908,7 +908,7 @@ def main(inject_iteration=None):
         A = np.array([4.]) # (SUBSET)
         B = fwhm_pix*ps_asec_per_pix*A # array of angular offsets where fake planets are to be injected
 
-        fake_params_pre_permute = {"angle_deg_EofN": [240],
+        fake_params_pre_permute = {"angle_deg_EofN": [120.,240.],
                                "rad_asec": list(B),
                                "ampl_linear_norm": [0.00014]}
 

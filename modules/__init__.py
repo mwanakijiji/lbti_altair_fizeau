4943c5c0f78eb83da901d03680e1c22d1d127caf
Eckhart <spalding@email.arizona.edu>
2020-06-24 10:49:31 -0700

    FWHM*7, 240 deg, w head start

--- modules/__init__.py
+++ modules/__init__.py
@@ -58,7 +58,7 @@ fwhm_4um_lbt_airy_pix = 1.028*lambda_over_D_pix # fwhm of a 4 um Airy PSF with t
 # (in linear units normalized to star; note these are STEP sizes)
 #del_amplitude_progression = np.array([0.5e-3,1e-4,5e-5,1e-5,5e-6,1e-6,5e-7,1e-7,5e-8,1e-8])
 
-del_amplitude_progression = np.array([5e-6,1e-6]) # truncated 2020 Apr. 2 and 9
+del_amplitude_progression = np.array([1e-6]) # truncated 2020 Apr. 2 and 9
 
 ## THIS IS THE STANDARD ONE IVE BEEN USING
 #del_amplitude_progression = np.array([1e-4,5e-5,1e-5,5e-6,1e-6]) # truncated 2020 Apr. 2 and 9

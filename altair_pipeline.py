e8088684caf265e8871200acb2d232a394062a5a
Eckhart <spalding@email.arizona.edu>
2020-07-18 16:44:40 -0700

    test

--- altair_pipeline.py
+++ altair_pipeline.py
@@ -97,7 +97,9 @@ lambda_over_B_KS_test.main(stripe_w_planet = 0,half_w_planet = "W",write_csv_bas
 lambda_over_B_contrast_curves.main(stripe_w_planet = 0,half_w_planet = "W",read_csv_basename = "test_0W.csv")
 
 lambda_over_B_KS_test.main(stripe_w_planet = 1,half_w_planet = "E",write_csv_basename = "test_1E.csv")
+'''
 lambda_over_B_contrast_curves.main(stripe_w_planet = 1,half_w_planet = "E",read_csv_basename = "test_1E.csv")
+'''
 lambda_over_B_KS_test.main(stripe_w_planet = 1,half_w_planet = "W",write_csv_basename = "test_1W.csv")
 lambda_over_B_contrast_curves.main(stripe_w_planet = 1,half_w_planet = "W",read_csv_basename = "test_1W.csv")
 

29509221a295da6fdc1f6dccb472913231a68c94
Eckhart <spalding@email.arizona.edu>
2020-09-06 14:29:31 -0600

    debug

--- modules/lambda_over_B_KS_test.py
+++ modules/lambda_over_B_KS_test.py
@@ -83,8 +83,8 @@ def return_residuals(array_loaded):
     #array_relevant[np.abs(array_loaded)<1e-10] = np.nan
     # (the below is a kludge, since the above method was not working well)
     array_relevant = np.copy(array_loaded)
-    array_relevant[:192,:] = np.nan
-    array_relevant[214:,:] = np.nan
+    array_relevant[:42,:] = np.nan
+    array_relevant[65:,:] = np.nan
     import ipdb; ipdb.set_trace()
     # convolve with a smoothing function? I've decided against it for now...
 

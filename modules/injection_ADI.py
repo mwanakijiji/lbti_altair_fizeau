02673e5cb49f72d97c5ab0c225090fe7a1c3ee69
Eckhart <spalding@email.arizona.edu>
2020-09-09 17:26:35 -0600

    50.32,70.782 degEoF, horiz tessel

--- modules/injection_ADI.py
+++ modules/injection_ADI.py
@@ -897,15 +897,15 @@ def main(inject_iteration=None):
         ps_asec_per_pix = np.float(config["instrum_params"]["LMIR_PS"]) # plate scale
         #A = np.arange(2,21) # FWHM numbers from center to edge of frame (FULL THING)
         #A = np.array([2,3,4,5,7,9,12,15,19]) # more sparse design as R increases (FULL THING)
-        #A = np.array([0.1,0.4,0.7,1.,1.3,1.7,2.0,2.3,2.6,2.9,3.2,4.,5.]) # (SUBSET)
-        A = np.array([0.1])
+        A = np.array([0.1,0.4,0.7,1.,1.3,1.7,2.0,2.3,2.6,2.9,3.2,4.,5.]) # (SUBSET)
+        #A = np.array([0.1])
         B = fwhm_pix*ps_asec_per_pix*A # array of angular offsets where fake planets are to be injected
 
         mag_array = [0,1.5,7,8,9,10] # if I want the flux array to be based on mag steps
         flux_array = np.divide(1.,np.power(10.,np.multiply(0.4,(mag_array))))
         #flux_array = [0]
 
-        fake_params_pre_permute = {"angle_deg_EofN": [0],
+        fake_params_pre_permute = {"angle_deg_EofN": [50.32,70.782],
                                "rad_asec": list(B),
                                "ampl_linear_norm": flux_array}
 
@@ -1218,7 +1218,7 @@ def main(inject_iteration=None):
         read_name_abs_fake_planet_PCA = str(config["data_dirs"]["DIR_PCA_CUBES_PSFS"]
                                             + "psf_PCA_vector_cookie_all_B_and_C_frames_pcaNum_100_host_resids.fits"),
         read_name_abs_pca_tesselation_pattern = str(config["data_dirs"]["DIR_OTHER_FITS"] +
-                                                    "mask_406x406_vertical_center_strip_width_2_FWHM_lamb_over_B.fits"),
+                                                    "mask_406x406_center_strip_width_2_FWHM_lamb_over_B.fits"),
         write_name_abs_cube_put_frames_into_it_simple = str(config["data_dirs"]["DIR_PCA_CUBES_PSFS"]
                                                             + "simple_synthetic_sci_frame_cube_A_and_D.fits"),
         write_name_abs_cube_A_PCA_vector = str(config["data_dirs"]["DIR_PCA_CUBES_PSFS"]

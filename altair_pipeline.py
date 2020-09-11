23b590b585af7987c7d893afe2fdb7e9e6d1668e
Eckhart <spalding@email.arizona.edu>
2020-09-11 15:17:26 -0600

    OVS test

--- altair_pipeline.py
+++ altair_pipeline.py
@@ -94,13 +94,12 @@ lambda_over_B_KS_test.main(stripe_w_planet = "0",half_w_planet = "E",write_csv_b
 #lambda_over_B_contrast_curves.main(stripe_w_planet = "0",half_w_planet = "E",read_csv_basename = "test_0E.csv")
 lambda_over_B_KS_test.main(stripe_w_planet = "0",half_w_planet = "W",write_csv_basename = "test01_20200911_0W.csv")
 #lambda_over_B_contrast_curves.main(stripe_w_planet = "0",half_w_planet = "W",read_csv_basename = "test_0W.csv")
-'''
 lambda_over_B_KS_test.main(stripe_w_planet = "0V",half_w_planet = "N",write_csv_basename = "test01_20200911_0VN.csv")
 '''
 #lambda_over_B_contrast_curves.main(stripe_w_planet = "0V",half_w_planet = "N",read_csv_basename = "test_0VN.csv")
 lambda_over_B_KS_test.main(stripe_w_planet = "0V",half_w_planet = "S",write_csv_basename = "test01_20200911_0VS.csv")
 #lambda_over_B_contrast_curves.main(stripe_w_planet = "0V",half_w_planet = "S",read_csv_basename = "test_0VS.csv")
-
+'''
 lambda_over_B_KS_test.main(stripe_w_planet = "1",half_w_planet = "E",write_csv_basename = "test01_20200911_1E.csv")
 #lambda_over_B_contrast_curves.main(stripe_w_planet = "1",half_w_planet = "E",read_csv_basename = "test_1E.csv")
 lambda_over_B_KS_test.main(stripe_w_planet = "1",half_w_planet = "W",write_csv_basename = "test01_20200911_1W.csv")
